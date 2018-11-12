f6109799ea29b9a7341746f242489b8f11b94d64
Eric Weiler <eric.weiler@northwestern.edu>
2018-11-12 14:11:22 -0600

    include rest and graphql endpoints

--- src/App.js
+++ src/App.js
@@ -1,28 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Amplify, { Analytics, Storage, API, graphqlOperation } from 'aws-amplify'
+import { withAuthenticator, S3Album } from 'aws-amplify-react'
+import aws_exports from './aws-exports'
 import './App.css';
 
+Amplify.configure(aws_exports)
+Storage.configure({ level: 'private' })
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+    }
+  }
+}`
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`
+
 class App extends Component {
+  uploadFile = (evt) => {
+    const file = evt.target.files[0]
+    const name = file.name
+    Storage.put(name, file).then(() => {
+      this.setState({ file: name })
+    })
+  }
+  componentDidMount() {
+    Analytics.record('Amplify_CLI')
+  }
+  todoMutation = async () => {
+    const todoDetails = {
+      name: 'Party tonight!',
+      description: 'Amplify CLI rocks!'
+    };
+    
+    const newEvent = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newEvent));
+  }
+
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  }
+
+  post = async () => {
+    console.log('calling api');
+    const response = await API.post('myitems', '/items', {
+      body: {
+        id: '1',
+        name: 'hello amplify!'
+      }
+    });
+    alert(JSON.stringify(response, null, 2));
+  }
+  get = async () => {
+    console.log('calling api');
+    const response = await API.get('myitems', '/items/object/1');
+    alert(JSON.stringify(response, null, 2));
+  }
+  list = async () => {
+    console.log('calling api');
+    const response = await API.get('myitems', '/items/1');
+    alert(JSON.stringify(response, null, 2));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+        <button onClick={this.post}>POST</button>
+        <button onClick={this.get}>GET</button>
+        <button onClick={this.list}>LIST</button>
+        
+        <S3Album level="private" path='' />
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
